cd9936f1174bf4676af735731ba1c3e73a2f1054
meti yonas <yohanistasfa1996@gmail.com>
2025-07-18 12:38:14 +0300

    dom selcting single element

---
+++ dom/script.js
@@ -0,0 +1,41 @@
+// console.dir(document); // to display the whole object structure in console
+// document.body.children[1].children[1].innerText = "yoni"; // make change to html by moving though its structure without changing html part
+
+// SELECTING SINGLE ELEMENT
+// getelemenet by id
+let sel = document.getElementById("one");
+console.log(sel.className);
+sel.className = "yellow";
+//queryselector
+console.log(document.querySelector("#one")); //select the first element only
+console.log(document.querySelector(".green"));
+console.log(document.querySelector("h1"));
+
+// SELECTING MULTIPLE ELEMENT
+//get element by classname = we can not loop
+let s = document.getElementsByClassName("green");
+console.log(s);
+console.log(s[0]);
+// s[1].className = "yellow";
+
+//get element bt tagname = we can loop
+console.log(document.getElementsByTagName("h1"));
+
+//query selctor all = we can loop
+console.log(document.querySelectorAll(".green"));
+
+// we can loop in query selector and tagname b/c it return nodelist
+var loop = document.querySelectorAll(".green");
+console.log(loop);
+for (i = 0; i < loop.length; i++) {
+  var el = loop[i];
+  el.className = "yellow";
+}
+
+// we can not loop in classname b/c it return htmldocumentation
+var loop = document.getElementsByClassName("green");
+console.log(loop);
+for (i = 0; i < loop.length; i++) {
+  var el = loop[i];
+  el.className = "yellow";
+}
